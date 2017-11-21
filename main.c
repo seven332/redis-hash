6bca90e00e5d0b1a99509fcab40b56e035843d48
seven332 <seven332@163.com>
2017-11-21 21:09:53 +0800

    Use custom DictType

--- main.c
+++ main.c
@@ -2,48 +2,40 @@
 #include <memory.h>
 
 #include "dict.h"
-#include "sds.h"
 
-uint64_t dictSdsHash(const void *key) {
-    return dictGenHashFunction((unsigned char*)key, sdslen((char*)key));
+uint64_t stringHash(const void *key) {
+    unsigned char *str = key;
+    unsigned long hash = 5381;
+    int c;
+
+    while (c = *str++)
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+    return hash;
 }
 
-int dictSdsKeyCompare(void *privdata, const void *key1,
-                      const void *key2)
-{
-    int l1,l2;
-    DICT_NOTUSED(privdata);
-
-    l1 = sdslen((sds)key1);
-    l2 = sdslen((sds)key2);
-    if (l1 != l2) return 0;
-    return memcmp(key1, key2, l1) == 0;
-}
-
-void dictSdsDestructor(void *privdata, void *val)
-{
-    DICT_NOTUSED(privdata);
-    sdsfree(val);
+int stringCompare(void *privdata, const void *key1, const void *key2) {
+    return strcmp(key1, key2) == 0;
 }
 
 int main() {
 
-    dictType clusterNodesDictType = {
-            dictSdsHash,                /* hash function */
+    dictType stringDictType = {
+            stringHash,                /* hash function */
             NULL,                       /* key dup */
             NULL,                       /* val dup */
-            dictSdsKeyCompare,          /* key compare */
-            dictSdsDestructor,          /* key destructor */
+            stringCompare,          /* key compare */
+            NULL,                       /* key destructor */
             NULL                        /* val destructor */
     };
 
-    dict* dict1 = dictCreate(&clusterNodesDictType, NULL);
+    dict* dict1 = dictCreate(&stringDictType, NULL);
 
     dictAdd(dict1, "1", "aaa");
     dictAdd(dict1, "2", "bbb");
     dictAdd(dict1, "3", "ccc");
 
-    printf("%s\n", dictFind(dict1, "1")->v);
+    printf("%s\n", dictFind(dict1, "3")->v);
 
     return 0;
 }
